c277b878567b6cc58dfb4270d86edf8d3a9fc230
JasonAsselin <45043553+JasonAsselin@users.noreply.github.com>
2020-06-11 16:36:36 -0400

    -created new .pug files for jason, mel and rob - updated index.pug for p tags - updated header.pug

--- Lab3/routes/index.js
+++ Lab3/routes/index.js
@@ -4,7 +4,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Welcome to my family members home page' });
+});
+
+/* GET Jason's page. */
+router.get('/jason', function (req, res) {
+    res.render('jason', { title: 'Welcome to Jason\'s page' });
+});
+
+/* GET Rob's page. */
+router.get('/rob', function (req, res) {
+    res.render('rob', { title: 'Welcome to Rob\'s homepage' });
+});
+
+/* GET Mel's page. */
+router.get('/mel', function (req, res) {
+    res.render('mel', { title: 'Welcome to Mel\'s homepage' });
 });
 
 module.exports = router;
